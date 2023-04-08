9376686bab0a2ff39e28968ecdb358b538e6d0db
aasad <aasad@qou.edu>
2023-04-08 10:05:49 +0300

    ali test

--- src/main/java/com/qu/finsys/controllers/UserController.java
+++ src/main/java/com/qu/finsys/controllers/UserController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api")
 //@SecurityRequirement(name = "FinSys App")
 public class UserController {
+
 	
 	@Autowired
 	private UserService userService;
